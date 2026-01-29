9b852c147cd39fca9274f08c84a094dc81f5a926
erda154 <erdamullabazi@gmail.com>
2026-01-29 21:27:28 +0100

    main js

---
+++ pages/lesson 26.html/main.js
@@ -0,0 +1,103 @@
+console.log("goodbye!!!");
+
+var x =10;
+var _value = "digital school";
+
+
+var a=50+50;
+console.log(a);
+
+var b = 20;
+var c = 30;
+var d= b + c;
+console.log(d);
+
+
+var e = 40;
+var f = 10;
+var g = e - f;
+console.log(g);
+
+var h = 100;
+var i = 5;
+var j = h \ i;
+console.log(j);
+
+var k = 10;
+var l = 4;
+var m = k  % l;
+console.log(m);
+
+var n = 99;
+n++;
+console.log(n);
+
+var o =101;
+o--;
+console.log(o);
+
+var p = 20;
+var q =p**2;
+console.log(q);
+
+var x = 5;
+
+var x =15;
+x-=5;
+console.log(x);
+
+var r = 35;
+r-=5;
+console.log(r);
+
+var s = 10;
+s*-7;
+console.log(s);
+
+
+var t = 200;
+t\=4;
+console.log(t);
+
+
+var u =10;
+u%=5;
+console.log(u);
+
+var v = 5;
+var w = 5;
+console.log(v == w);
+
+var x = 6;
+var y = 7;
+console.log(x != y);
+
+var num1 = 100;
+var num2 = 10;
+console.log(num1 > num2);
+
+var num3 = 1;
+var num4 = 2;
+console.log(num3 > num4);
+
+var num5 = 123;
+var num6  = "123";
+console.log(num5 === num6);
+
+var firstname = "Erda";
+var lastname =  "isma";
+var fullname = firstname + "" + lastname;
+console.log(fullname);
+
+var school= "Digital school";
+console.log("hello from ${school}");
+
+var age = 24;
+console.log(age>=18 && age <= 65);
+
+var var1 =5;
+var var2 = 6;
+console.log(var1 == 5 || var2 == 5)
+
+
+
