fc1b57ac513795703198adf5f44265ed565037d3
erda154 <erdamullabazi@gmail.com>
2026-02-12 20:14:55 +0100

    j

---
+++ lesson 29/main.js
@@ -0,0 +1,44 @@
+var programingLanguages = {"Javascript" , "PHP" , "Python"}
+
+console.log (programingLanguages);
+
+console.log(programingLanguages{0})
+
+programingLanguages.push(Javascript);
+
+programingLanguages.pop();
+
+programingLanguages.unshift("c#")
+
+programingLanguages.shift();
+
+programingLanguages.splice(0,2  "Ruby")
+
+console.log(Math.random()*5);
+
+console.log(Math.floor(Math.random()*5));
+
+var students = ["Joni", "Erda", "Endrit", "Bledion"];
+
+var [s1, s2, s3, s4] =students
+
+console.log(s1);
+console.log(s2);
+console.log(s3);
+console.log(s4);
+
+var places  = ["Berlin", "Paris", "New jearsy", "London"]; 
+
+var [firstplace, secondplace] = places;
+
+console.log(secondplace):
+
+var numbers = [1, 2, 3,4,5,6,7,8,9,10]
+
+var [ first,second, ...otherNumbers] = numbers;
+
+console.log(first)
+
+console.log(second)
+
+console.log(otherNumbers.toString());
